5d18ffe5ef0b922a6c8aebe7db9d2a19061fc162
Matthew Randle <exportknowledge.matthew@gmail.com>
2019-07-12 14:02:14 +0100

    Added why choose me section.

---
+++ Index.js
@@ -0,0 +1,9 @@
+const contact = document.getElementsByClassName("contact")[0];
+
+function scrollToContact() {
+    contact.scrollIntoView();
+}
+
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
