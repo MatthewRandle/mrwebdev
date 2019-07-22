c0f00beceae1d39542228ebc5efc12c0f3d00d03
Matthew Randle <exportknowledge.matthew@gmail.com>
2019-07-22 13:16:10 +0100

    Added footer to legal pages

---
+++ sendMessage.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    $subject = "New message from $name, $email";
+    $to="enquiry@mrweb.dev";
+    $headers="MIME_VERSION: 1.0 \r\n Content-type:text/html;charset=UTF-8";
+?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            if(isset($_POST["submit"])) {
+                if(mail($to, $subject, $message, $headers)) {
+                    echo "sent";
+                }
+                else {
+                    echo "not sent";
+                }
+            }
+        ?>
+    </body>
+</html>
