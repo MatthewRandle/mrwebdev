9a6b83be95f4958040a71a4090d9d10c41893f4d
Matthew Randle <exportknowledge.matthew@gmail.com>
2019-07-24 11:56:45 +0100

    Added sendgrid email API

--- sendMessage.php
+++ sendMessage.php
@@ -1,27 +1,28 @@
 <?php
+    require("vendor/autoload.php");
+
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
     $subject = "New message from $name, $email";
     $to="contact@misterweb.dev";
     $headers="MIME_VERSION: 1.0 \r\n Content-type:text/html;charset=UTF-8 \r\n From: enquiry@misterweb.dev";
-?>
 
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php
-            if(isset($_POST["submit"])) {
-                if(mail($to, $subject, $message, $headers)) {
-                    echo "sent";
-                }
-                else {
-                    echo "not sent";
-                }
-            }
-            else {
-                echo "No submit";
-            }
-        ?>
-    </body>
-</html>
+    $mail = new \SendGrid\Mail\Mail();
+    $mail->setFrom("enquiry@misterweb.dev", "New Enquiry");
+    $mail->setSubject($subject);
+    $mail->addTo("randle.matthew@hotmail.co.uk");
+    $mail->addContent("text/plain", $message);
+
+    $sendgrid = new \SendGrid(getenv(SENDGRID_KEY));
+
+    try {
+        $response = $sendgrid->send($mail);
+        if($response != 202) {
+            
+        }
+    }
+    catch(Exception $e) {
+        echo $e->getMessage();
+    }
+?>
