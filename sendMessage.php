3d1d610ebd71188a53edf59d5870d10a80b126e2
Matthew Randle <exportknowledge.matthew@gmail.com>
2019-07-24 13:38:40 +0100

    Re-added email script

--- sendMessage.php
+++ sendMessage.php
@@ -1,18 +1,16 @@
 <?php
-    /* require("vendor/autoload.php");
+    require("vendor/autoload.php");
     $dotenv = Dotenv\Dotenv::create("/var/www");
-    $dotenv->load(); */
+    $dotenv->load();
 
-    /* $name = $_POST["name"];
+    $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
     $subject = "New message from $name, $email";
     $to="contact@misterweb.dev";
-    $headers="MIME_VERSION: 1.0 \r\n Content-type:text/html;charset=UTF-8 \r\n From: enquiry@misterweb.dev"; */
+    $headers="MIME_VERSION: 1.0 \r\n Content-type:text/html;charset=UTF-8 \r\n From: enquiry@misterweb.dev";
 
-   
-
-    /* $mail = new \SendGrid\Mail\Mail();
+    $mail = new \SendGrid\Mail\Mail();
     $mail->setFrom("enquiry@misterweb.dev", "New Enquiry");
     $mail->setSubject($subject);
     $mail->addTo($to);
@@ -29,5 +27,5 @@
     }
     catch(Exception $e) {
         echo $e->getMessage();
-    } */
+    }
 ?>
